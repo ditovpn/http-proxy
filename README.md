0a243d003ae0fc51cb684fba962ac6268ee4bb51
DITO VPN <142469327+ditovpn@users.noreply.github.com>
2024-01-14 18:34:04 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# http-proxy
+# Squid Proxy Installer
+
+Auto install Squid 3 proxy on following linux OS.
+
+* Ubuntu 14.04, 16.04, 18.04, 20.04, 22.04
+* Debian 8, 9, 10
+* CentOS 7, 8
+
+
+## Install Squid
+
+To install, run the script
+
+```
+wget https://raw.githubusercontent.com/devmaxxph/squid-proxy/master/squid3-install.sh -O squid3-install.sh
+sudo bash squid3-install.sh
+```
+
+
+# Create Users
+
+To create users, run
+
+```
+squid-add-user
+```
+
+OR run following commands
+
+```
+sudo /usr/bin/htpasswd -b -c /etc/squid/passwd USERNAME_HERE PASSWORD_HERE
+```
+
+To update password for am existing user, run
+
+```
+sudo /usr/bin/htpasswd /etc/squid/passwd USERNAME_HERE
+```
+
+replace USERNAME_HERE and PASSWORD_HERE with your desired user name and password.
+
+Reload squid proxy
+
+```
+sudo systemctl reload squid
+```
+
+# Configure Multiple IP Address
+
+NOTE: This is only needed if you have more than one IP on your server.
+
+Before you can configure squid to use muliple IP address, you need to add IP to your server and you should be able to connect to server using these IPs.
+
+Once IP added to your server, you can configure it to use with squid proxy by running following command
+
+```
+wget https://raw.githubusercontent.com/devmaxxph/squid-proxy/master/squid-conf-ip.sh
+sudo bash squid-conf-ip.sh
+```
+
+# Change Squid Proxy Port
+
+sed -i 's/^http_port.*$/http_port 5555/g'  /etc/squid/squid.conf
+
+# Support
+
+If you are looking for paid support, contact me
+
+https://t.me/devmaxx
---
+++ squid-uninstall.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+############################################################
+# Squid Proxy Installer
+# Author: Ian
+# Email: ian@dmsmarters.com
+# Github: https://github.com/devmaxxph/squid-proxy
+# Web: https://dmsmarters.com
+############################################################
+# For paid support, contact
+# https://t.me/devmaxx
+############################################################
+
+if [ `whoami` != root ]; then
+	echo "ERROR: You need to run the script as user root or add sudo before command."
+	exit 1
+fi
+
+if [ ! -f /usr/local/bin/sok-find-os ]; then
+    echo "/usr/local/bin/sok-find-os not found"
+    exit 1
+fi
+
+SOK_OS=$(/usr/local/bin/sok-find-os)
+
+if [ $SOK_OS == "ubuntu2204" ]; then
+    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
+    rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu2004" ]; then
+    /usr/bin/apt -y remove --purge squid*
+    rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu1804" ]; then
+    /usr/bin/apt -y remove --purge squid3
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu1604" ]; then
+    /usr/bin/apt -y remove --purge squid3
+    /bin/rm -rf /etc/squid3/
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "ubuntu1404" ]; then
+    /usr/bin/apt remove --purge squid3 -y
+    /bin/rm -rf /etc/squid3/
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "debian8" ]; then
+    echo "Uninstalling Squid Proxy on Debian 8"
+    /usr/bin/apt -y remove --purge squid
+    /bin/rm -rf /etc/squid3/
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "debian9" ]; then
+    echo "Uninstalling Squid Proxy on Debian 9"
+    /usr/bin/apt -y remove --purge squid
+    /bin/rm -rf /etc/squid/
+    /bin/rm -rf /var/spool/squid
+elif [ $SOK_OS == "debian10" ]; then
+    echo "Uninstalling Squid Proxy on Debian 10"
+    /usr/bin/apt -y remove --purge squid squid-common squid-langpack
+    /bin/rm -rf /etc/squid/
+    /bin/rm -rf /var/spool/squid
+elif [ $SOK_OS == "centos7" ]; then
+    yum remove squid -y
+    /bin/rm -rf /etc/squid/
+elif [ $SOK_OS == "centos8" ]; then
+    yum remove squid -y
+    /bin/rm -rf /etc/squid/
+else
+    echo "OS NOT SUPPORTED.\n"
+    echo "Contact https://serverok.in/contact to add support for your OS."
+    exit 1;
+fi
+
+rm -f /usr/local/bin/squid-add-user > /dev/null 2>&1
+
+echo 
+echo 
+echo "Squid Proxy uninstalled."
+echo "Thank you for using serverok.in squid proxy installer"
+echo "If you want to reinstall Squid Proxy Server, check https://serverok.in/squid"
+echo 
